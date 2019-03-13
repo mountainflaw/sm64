dd5ee80010e2804e0cd3a3cf74775317d8db1360
mountainflaw <38231712+mountainflaw@users.noreply.github.com>
2019-03-12 22:05:51 -0400

    add point light support

--- obelisk.h
+++ obelisk.h
@@ -14,7 +14,7 @@
 // includes
 
 #include "area.h" // gCurrLevelNum
-
+#include "level_update.h"
 // define modes for setMarioLightShadingMode
 
 #define SHADE_100    1
@@ -105,17 +105,80 @@ extern Light2 *mario_seg4_light_04000078;
 
 // level lights; make sure to glabel these too if you use them
 
+void setMarioLightShadingMode(f32 mode) // used to set mario's shading to one of the four predetermined modes
+{
+	Light2 *ptr;
+	ptr = segmented_to_virtual(&mario_seg4_light_04000000);
+	ptr->byte[2] = VAL1 * mode;
+	ptr->byte[6] = VAL1 * mode;
+	ptr = segmented_to_virtual(&mario_seg4_light_04000018);
+	ptr->byte[0] = VAL1 * mode;
+	ptr->byte[4] = VAL1 * mode;
+	ptr = segmented_to_virtual(&mario_seg4_light_04000030);
+	ptr->byte[0] = VAL1 * mode;
+	ptr->byte[1] = VAL1 * mode;
+	ptr->byte[2] = VAL1 * mode;
+	ptr->byte[4] = VAL1 * mode;
+	ptr->byte[5] = VAL1 * mode;
+	ptr->byte[6] = VAL1 * mode;
+	ptr = segmented_to_virtual(&mario_seg4_light_04000048);
+	ptr->byte[0] = VAL2 * mode;
+	ptr->byte[1] = VAL3 * mode;
+	ptr->byte[2] = VAL4 * mode;
+	ptr->byte[4] = VAL2 * mode;
+	ptr->byte[5] = VAL3 * mode;
+	ptr->byte[6] = VAL4 * mode;
+	ptr = segmented_to_virtual(&mario_seg4_light_04000078);
+	ptr->byte[0] = VAL2 * mode;
+	ptr->byte[1] = VAL7 * mode;
+	ptr->byte[4] = VAL2 * mode;
+	ptr->byte[5] = VAL7 * mode;
+}
 
+void point_light(int x, int y, int z, int size, float shade, int size1, float shade1)
+{
+    // outer region
+
+    if (gMarioState->pos[0] >= (x - size1) && gMarioState->pos[0] <= x + size1 &&
+    gMarioState->pos[1] >= (y - size1) && gMarioState->pos[1] <= y + size1 &&
+    gMarioState->pos[2] >= (z - size1) && gMarioState->pos[2] <= z + size1)
+        setMarioLightShadingMode(shade1);
+
+    // inner region
+
+    if (gMarioState->pos[0] >= (x - size) && gMarioState->pos[0] <= x + size &&
+    gMarioState->pos[1] >= (y - size) && gMarioState->pos[1] <= y + size &&
+    gMarioState->pos[2] >= (z - size) && gMarioState->pos[2] <= z + size)
+        setMarioLightShadingMode(shade);
+}
 
 #define DEBUG
 void setLevelShading(void) // sets the shading for mario based on level number
 {
 #ifdef DEBUG
-	print_text_fmt_int(40, 60, "AREA: %d", gCurrAreaIndex);
+	print_text_fmt_int(40, 80, "AREA: %d", gCurrAreaIndex);
+    print_text_fmt_int(40, 60, "X: %d", gMarioState->pos[0]);
+    print_text_fmt_int(40, 40, "Y: %d", gMarioState->pos[1]);
+    print_text_fmt_int(40, 20, "Z: %d", gMarioState->pos[2]);
+
 #endif
 	switch(gCurrLevelNum) 
 	{
 		case BBH:
+        	setMarioLightShadingMode(SHADE_25);
+
+            point_light(200, 0, 2300, 200, SHADE_100, 400, SHADE_50); // outside
+            point_light(1000, 0, 2300, 200, SHADE_100, 400, SHADE_50);
+
+            point_light(200, 0, 1500, 150, SHADE_75, 200, SHADE_50); // middle two
+            point_light(200, 0, 700,  150, SHADE_75, 200, SHADE_50);
+
+            point_light(450, 0, 300,  150, SHADE_75, 200, SHADE_50); // three
+            point_light(1550, 0, 300,  150, SHADE_75, 200, SHADE_50);
+            point_light(1800, 0, 1500,  150, SHADE_75, 200, SHADE_50);
+
+            point_light(1500, 0, 1750,  100, SHADE_50, 150, SHADE_25); // moonlight
+            break;
 		case HMC:
 			setMarioLightShadingMode(SHADE_25);
 			break;
@@ -147,33 +210,4 @@ void setLevelShading(void) // sets the shading for mario based on level number
 	}
 }
 
-void setMarioLightShadingMode(float mode) // used to set mario's shading to one of the four predetermined modes
-{
-	Light2 *ptr;
-	ptr = segmented_to_virtual(&mario_seg4_light_04000000);
-	ptr->byte[2] = VAL1 * mode;
-	ptr->byte[6] = VAL1 * mode;
-	ptr = segmented_to_virtual(&mario_seg4_light_04000018);
-	ptr->byte[0] = VAL1 * mode;
-	ptr->byte[4] = VAL1 * mode;
-	ptr = segmented_to_virtual(&mario_seg4_light_04000030);
-	ptr->byte[0] = VAL1 * mode;
-	ptr->byte[1] = VAL1 * mode;
-	ptr->byte[2] = VAL1 * mode;
-	ptr->byte[4] = VAL1 * mode;
-	ptr->byte[5] = VAL1 * mode;
-	ptr->byte[6] = VAL1 * mode;
-	ptr = segmented_to_virtual(&mario_seg4_light_04000048);
-	ptr->byte[0] = VAL2 * mode;
-	ptr->byte[1] = VAL3 * mode;
-	ptr->byte[2] = VAL4 * mode;
-	ptr->byte[4] = VAL2 * mode;
-	ptr->byte[5] = VAL3 * mode;
-	ptr->byte[6] = VAL4 * mode;
-	ptr = segmented_to_virtual(&mario_seg4_light_04000078);
-	ptr->byte[0] = VAL2 * mode;
-	ptr->byte[1] = VAL7 * mode;
-	ptr->byte[4] = VAL2 * mode;
-	ptr->byte[4] = VAL7 * mode;
-}
 #endif
